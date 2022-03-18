b7ab4655b6d83a2c011d8bdfb21e0ce6de8761ed
Juan <juanluismunozbarraza@gmail.com>
2022-03-18 18:44:52 -0300

    base FrontEnd

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //Permite establecer variables de entorno
 require('dotenv').config();
 
+const express = require('express');
 
 const Server = require('./models/server.model');
-
 const server = new Server();
 
+
 server.listen();
+
